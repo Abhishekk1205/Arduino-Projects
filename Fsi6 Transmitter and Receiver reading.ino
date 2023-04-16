8400f4665263eb66da6e663f137b5d2f9af2dc99
Abhishek Kumar Gupta <117657114+Abhishekk1205@users.noreply.github.com>
2023-04-16 23:13:47 +0530

    Update Fsi6 Transmitter and Receiver reading.ino

--- Fsi6 Transmitter and Receiver reading.ino
+++ Fsi6 Transmitter and Receiver reading.ino
@@ -1,33 +1,27 @@
 int ch1,ch2,ch3,ch4,ch5,ch6;
 void setup() 
 {
-  // put your setup code here, to run once:
 pinMode(3,INPUT);
 pinMode(5,INPUT);
 pinMode(6,INPUT);
 pinMode(9,INPUT);
 pinMode(10,INPUT);
-pinMode(11,INPUT);
-
+pinMode(11,INPUT)
 Serial.begin(9600);
 }
-
 void loop() 
 {
-  // put your main code here, to run repeatedly:
 ch1= pulseIn(3,HIGH);
 ch2= pulseIn(5,HIGH);
 ch3= pulseIn(6,HIGH);
 ch4= pulseIn(9,HIGH);
 ch5= pulseIn(10,HIGH);
 ch6= pulseIn(11,HIGH);
-//ch1= pulseIn(3,HIGH);
-//Serial.println("ch1");
+
 Serial.print((ch1-986)/10);Serial.print("\t");
 Serial.print((ch2-986)/10);Serial.print("\t");
 Serial.print((ch3-986)/10);Serial.print("\t");
 Serial.print((ch4-986)/10);Serial.print("\t");
 Serial.print((ch5-986)/10);Serial.print("\t");
-Serial.print((ch6-986)/10);Serial.print("\t");
-Serial.println();
+Serial.println((ch6-986)/10);
 }
